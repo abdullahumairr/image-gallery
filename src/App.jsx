2e255eb5e25f36fd29c83b48774d9088b9b74bd1
Abdullah Umair <abdulumair320@gmail.com>
2025-08-04 18:37:54 +0800

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Gallery } from "./gallery";
+import "./index.css";
+// import "./style.css";
+
+function App() {
+  return (
+    <>
+      <Gallery></Gallery>
+    </>
+  );
+}
+
+export default App;
